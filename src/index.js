55984eb20b8994fe79c5f65c5067962471a2aa3e
Federico Massa <fedemassa91@gmail.com>
2021-03-26 00:08:15 +0100

    Setup local server

---
+++ src/index.js
@@ -0,0 +1,9 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+const title = "Nice";
+
+ReactDOM.render(
+    <h2>{title}</h2>,
+    document.getElementById('app')
+);
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+ 
+module.exports = {
+  entry: path.resolve(__dirname, './src/index.js'),
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+    ],
+  },
+  mode: "development",
+  resolve: {
+    extensions: ['*', '.js', '.jsx'],
+  },
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'bundle.js',
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, './dist'),
+    port: 9000
+  },
+};
